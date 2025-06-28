706dbf0d4ed0f5885cfb6dfa2c82de8242d3a60c
Luis Nyaupane <neupaneluis07@gmail.com>
2025-06-28 08:52:02 +0545

    Add files via upload

---
+++ Gopanyacare/meanpage/reproductive/reproductive.js
@@ -0,0 +1,26 @@
+function openSection(id) {
+  // Show simple feedback for now
+  alert("You clicked: " + id);
+
+  // Later, you can route to another page like:
+  // window.location.href = id + ".html";
+}
+
+function goToReproductiveRightsPage() {
+  window.location.href = "./reproductiveRights/reproductiveRights.html";
+}
+function goToDoctorPage() {
+  window.location.href = "./reproductiveDoctor/reproductiveDoctor.html";
+}
+function goToMythPage() {
+  window.location.href = "./reproductiveMyth/reproductiveMyth.html";
+}
+function goToGovermentPage() {
+  window.location.href = "./reproductiveGoverment/reproductiveGoverment.html";
+}
+function goToLgbtqPage() {
+  window.location.href = "./reproductiveLgbtq/reproductiveLgbtq.html";
+}
+function goToWomePage() {
+  window.location.href = "./reproductiveWome/reproductiveWome.html";
+}
